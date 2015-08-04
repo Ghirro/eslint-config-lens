ba31a6bd4257bbba2ae006692e271180339a205b
Paolo Moretti <paolo.moretti85@gmail.com>
2015-08-04 13:29:28 +0100

    Indent switch cases with 2 spaces https://github.com/eslint/eslint/blob/master/docs/rules/indent.md

--- eslintrc.js
+++ eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   rules: {
     "block-scoped-var": 0,
     "func-names": 0,
+    "indent": [2, 2, {"SwitchCase": 1}], // 2 space indentation in switch cases
     "no-param-reassign": 0,
     "no-console": 0,
     "no-else-return": 0,
